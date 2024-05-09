bf0599ece18bd36e92cab15995c9ad810b393bab
MathiasAugustesen <mathiasoaug@gmail.com>
2024-05-09 13:00:16 +0200

    Move negamax to bottom and adjust visibility

--- src/search.rs
+++ src/search.rs
@@ -2,29 +2,6 @@ use crate::board_elements::ChessMove;
 use crate::board_state::BoardState;
 use crate::move_generation::generate_pseudo_moves_for_player;
 use crate::move_ordering::move_sort;
-pub fn negamax(board_state: &BoardState, depth: u8, alpha: i32, beta: i32) -> i32 {
-    if depth == 0 {
-        return board_state.eval * board_state.to_move.signum();
-    }
-    let mut best_eval = -i32::MAX;
-    let mut available_pseudo_moves = generate_pseudo_moves_for_player(board_state);
-
-    available_pseudo_moves.sort_by_cached_key(|&mov| -move_sort(&board_state, mov));
-    for mov in available_pseudo_moves {
-        let mut copy_board = board_state.clone();
-        copy_board.make_move(mov);
-        if !copy_board.is_valid_move() {
-            continue;
-        }
-        let eval = -negamax(&copy_board, depth - 1, -beta, -alpha);
-        let alpha = alpha.max(eval);
-        best_eval = eval.max(best_eval);
-        if alpha >= beta {
-            break;
-        }
-    }
-    best_eval
-}
 
 pub fn search(board_state: &BoardState, depth: u8) -> (i32, Option<ChessMove>) {
     let mut alpha = -i32::MAX;
@@ -49,4 +26,28 @@ pub fn search(board_state: &BoardState, depth: u8) -> (i32, Option<ChessMove>) {
         alpha = alpha.max(eval);
     }
     (best_eval, best_move)
+}
+
+fn negamax(board_state: &BoardState, depth: u8, alpha: i32, beta: i32) -> i32 {
+    if depth == 0 {
+        return board_state.eval * board_state.to_move.signum();
+    }
+    let mut best_eval = -i32::MAX;
+    let mut available_pseudo_moves = generate_pseudo_moves_for_player(board_state);
+
+    available_pseudo_moves.sort_by_cached_key(|&mov| -move_sort(&board_state, mov));
+    for mov in available_pseudo_moves {
+        let mut copy_board = board_state.clone();
+        copy_board.make_move(mov);
+        if !copy_board.is_valid_move() {
+            continue;
+        }
+        let eval = -negamax(&copy_board, depth - 1, -beta, -alpha);
+        let alpha = alpha.max(eval);
+        best_eval = eval.max(best_eval);
+        if alpha >= beta {
+            break;
+        }
+    }
+    best_eval
 }
